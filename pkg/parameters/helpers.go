d0518754387166eb5c88476c05b8af51b5606fe2
Dzmitry Afanasenka <dzmitry.afanasenka@itechart-group.com>
2020-04-15 15:08:53 +0300

    Changed priority for parameters. Created a few helpers for the parameters package.
    Now the priority is the following:
    1. Flag
    2. Env
    3. AWS Secrets Manager.

--- pkg/parameters/helpers.go
+++ pkg/parameters/helpers.go
@@ -1,17 +1,13 @@
 package parameters
 
 import (
+	"flag"
+	fl "github.com/barchart/barchart-common-go/pkg/parameters/flags"
+	"os"
 	"strconv"
 )
 
-func isAWSSecret(secretManager []bool) bool {
-	awsSecret := false
-	if len(secretManager) > 0 && secretManager[0] == true {
-		awsSecret = true
-	}
-	return awsSecret
-}
-
+// convertStrings converts a string to a typeValue and returns it
 func convertString(str string, typeValue string) interface{} {
 	switch typeValue {
 	case boolType:
@@ -51,4 +47,77 @@ func convertString(str string, typeValue string) interface{} {
 	}
 
 	return str
+}
+
+// getAWSSecretsRegion gets AwsRegionSecrets value from a AWS-REGION-SECRETS flag or env variable and returns it
+func getAWSSecretsRegion() string {
+	flg := flag.Lookup(AwsRegionSecrets)
+	region := flg.Value.String()
+	if !flg.Value.(*fl.StringValue).IsSet() {
+		region = os.Getenv(AwsRegionSecrets)
+		if region == "" {
+			region = flg.DefValue
+		}
+	}
+
+	return region
+}
+
+// getValueFromAWSSecretsManager returns a parameter value from the AWS Secrets Manager
+func getValueFromAWSSecretsManager(param parameter) interface{} {
+	if param.AWSSecret {
+		if sm != nil && smError == nil {
+			value, _, err := sm.GetValue(param.Name)
+			if err == nil {
+				return convertString(value, param.valueType)
+			}
+		}
+	}
+
+	return nil
+}
+
+// getValueFromFlag returns the value of the desired type from the flag
+func getValueFromFlag(flg *flag.Flag, typeValue string) (interface{}, bool) {
+	switch typeValue {
+	case boolType:
+		{
+			return flg.Value.(*fl.BoolValue).Get(), flg.Value.(*fl.BoolValue).IsSet()
+		}
+	case float64Type:
+		{
+			return flg.Value.(*fl.Float64Value).Get(), flg.Value.(*fl.Float64Value).IsSet()
+		}
+	case intType:
+		{
+			return flg.Value.(*fl.IntValue).Get(), flg.Value.(*fl.IntValue).IsSet()
+		}
+	case int64Type:
+		{
+			return flg.Value.(*fl.Int64Value).Get(), flg.Value.(*fl.Int64Value).IsSet()
+		}
+	case stringType:
+		{
+			return flg.Value.(*fl.StringValue).Get(), flg.Value.(*fl.StringValue).IsSet()
+		}
+	case uintType:
+		{
+			return flg.Value.(*fl.UintValue).Get(), flg.Value.(*fl.UintValue).IsSet()
+		}
+	case uint64Type:
+		{
+			return flg.Value.(*fl.Uint64Value).Get(), flg.Value.(*fl.Uint64Value).IsSet()
+		}
+	}
+
+	return nil, false
+}
+
+// isAWSSecret returns is AWS Secrets Manager argument was provided
+func isAWSSecret(awsSecret []bool) bool {
+	isAwsSecret := false
+	if len(awsSecret) > 0 && awsSecret[0] == true {
+		isAwsSecret = true
+	}
+	return isAwsSecret
 }
--- pkg/parameters/parameters.go
+++ pkg/parameters/parameters.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"github.com/barchart/barchart-common-go/pkg/configuration"
 	_ "github.com/barchart/barchart-common-go/pkg/configuration"
-	fl "github.com/barchart/barchart-common-go/pkg/parameters/flags"
+	"github.com/barchart/barchart-common-go/pkg/configuration/aws/secretsmanager"
+	"github.com/barchart/barchart-common-go/pkg/parameters/flags"
+	"log"
 	"os"
 	"strings"
 )
@@ -22,6 +24,11 @@ type parameter struct {
 type parameters struct {
 	collection map[string]parameter
 }
+
+var (
+	sm      *secretsmanager.SecretsManager
+	smError error
+)
 
 func New() *parameters {
 	return &parameters{collection: map[string]parameter{}}
@@ -41,7 +48,7 @@ func (p *parameters) AddBool(name string, value bool, usage string, required boo
 		valueType:    boolType,
 	}
 
-	fl.Bool(name, value, usage)
+	flags.Bool(name, value, usage)
 }
 
 func (p *parameters) AddFloat64(name string, value float64, usage string, required bool, awsSecret ...bool) {
@@ -54,7 +61,7 @@ func (p *parameters) AddFloat64(name string, value float64, usage string, requir
 		valueType:    float64Type,
 	}
 
-	fl.Float64(name, value, usage)
+	flags.Float64(name, value, usage)
 }
 
 func (p *parameters) AddInt(name string, value int, usage string, required bool, awsSecret ...bool) {
@@ -67,7 +74,7 @@ func (p *parameters) AddInt(name string, value int, usage string, required bool,
 		valueType:    intType,
 	}
 
-	fl.Int(name, value, usage)
+	flags.Int(name, value, usage)
 }
 
 func (p *parameters) AddInt64(name string, value int64, usage string, required bool, awsSecret ...bool) {
@@ -80,7 +87,7 @@ func (p *parameters) AddInt64(name string, value int64, usage string, required b
 		valueType:    int64Type,
 	}
 
-	fl.Int64(name, value, usage)
+	flags.Int64(name, value, usage)
 }
 
 func (p *parameters) AddString(name string, value string, usage string, required bool, awsSecret ...bool) {
@@ -93,7 +100,7 @@ func (p *parameters) AddString(name string, value string, usage string, required
 		valueType:    stringType,
 	}
 
-	fl.String(name, value, usage)
+	flags.String(name, value, usage)
 }
 
 func (p *parameters) AddUint(name string, value uint, usage string, required bool, awsSecret ...bool) {
@@ -106,7 +113,7 @@ func (p *parameters) AddUint(name string, value uint, usage string, required boo
 		valueType:    uintType,
 	}
 
-	fl.Uint(name, value, usage)
+	flags.Uint(name, value, usage)
 }
 
 func (p *parameters) AddUint64(name string, value uint64, usage string, required bool, awsSecret ...bool) {
@@ -118,33 +125,22 @@ func (p *parameters) AddUint64(name string, value uint64, usage string, required
 		AWSSecret:    isAWSSecret(awsSecret),
 		valueType:    uint64Type,
 	}
-	fl.Uint64(name, value, usage)
-}
-
-func getAWSSecretsRegion() string {
-	flg := flag.Lookup(AwsRegionSecrets)
-	region := flg.Value.String()
-	if !flg.Value.(*fl.StringValue).IsSet() {
-		region = os.Getenv(AwsRegionSecrets)
-		if region == "" {
-			region = flg.DefValue
-		}
-	}
-
-	return region
+	flags.Uint64(name, value, usage)
 }
 
 func (p parameters) Parse() map[string]interface{} {
 	result := make(map[string]interface{})
 	missing := make([]string, 0, 1)
 
-	fl.String(AwsRegionSecrets, "us-east-1", "The AWS Secrets Manager region")
+	flags.String(AwsRegionSecrets, "us-east-1", "The AWS Secrets Manager region")
 
 	flag.Parse()
 
 	config := configuration.GetConfig()
 	config.SetSecretsManager(getAWSSecretsRegion())
-	sm, smError := configuration.GetConfig().GetSecretsManager()
+	smm, smmError := configuration.GetConfig().GetSecretsManager()
+	sm = &smm
+	smError = smmError
 
 	if p.collection == nil {
 		panic("parameters wasn't added")
@@ -152,195 +148,31 @@ func (p parameters) Parse() map[string]interface{} {
 
 	for _, param := range p.collection {
 		flg := flag.Lookup(param.Name)
-		flagValueString := flg.Value.String()
-		flagValue := convertString(flagValueString, param.valueType)
+		value, isSet := getValueFromFlag(flg, param.valueType)
 
-		envValueString := os.Getenv(param.Name)
-		envValue := convertString(envValueString, param.valueType)
-
-		switch param.valueType {
-		case boolType:
-			{
-				if param.AWSSecret {
-					if smError == nil {
-						value, _, err := sm.GetValue(param.Name)
-						if err == nil {
-							result[param.Name] = convertString(value, param.valueType)
-							continue
-						}
-					}
-				}
-
-				if flg.Value.(*fl.BoolValue).IsSet() {
-					result[param.Name] = flagValue
+		if !isSet {
+			envValueString := os.Getenv(param.Name)
+			if envValueString != "" {
+				envValue := convertString(envValueString, param.valueType)
+				log.Printf("PARAM: %v: %T", param.Name, envValue)
+				result[param.Name] = envValue
+			} else {
+				secretValue := getValueFromAWSSecretsManager(param)
+				if secretValue != nil {
+					log.Printf("PARAM: %v: %T", param.Name, secretValue)
+					result[param.Name] = secretValue
 				} else {
-					if envValueString != "" {
-						result[param.Name] = envValue
+					if param.Required {
+						missing = append(missing, param.Name)
 					} else {
-						if param.Required {
-							missing = append(missing, param.Name)
-						} else {
-							result[param.Name] = flagValue
-						}
+						log.Printf("PARAM: %v: %T", param.Name, value)
+						result[param.Name] = value
 					}
 				}
 			}
-		case float64Type:
-			{
-				if param.AWSSecret {
-					if smError == nil {
-						value, _, err := sm.GetValue(param.Name)
-						if err == nil {
-							result[param.Name] = convertString(value, param.valueType)
-							continue
-						}
-					}
-				}
-
-				if flg.Value.(*fl.Float64Value).IsSet() {
-					result[param.Name] = flagValue
-				} else {
-					if envValueString != "" {
-						result[param.Name] = envValue
-					} else {
-						if param.Required {
-							missing = append(missing, param.Name)
-						} else {
-							result[param.Name] = flagValue
-						}
-					}
-				}
-			}
-		case intType:
-			{
-				if param.AWSSecret {
-					if smError == nil {
-						value, _, err := sm.GetValue(param.Name)
-						if err == nil {
-							result[param.Name] = convertString(value, param.valueType)
-							continue
-						}
-					}
-				}
-
-				if flg.Value.(*fl.IntValue).IsSet() {
-					result[param.Name] = flagValue
-				} else {
-					if envValueString != "" {
-						result[param.Name] = envValue
-					} else {
-						if param.Required {
-							missing = append(missing, param.Name)
-						} else {
-							result[param.Name] = flagValue
-						}
-					}
-				}
-			}
-		case int64Type:
-			{
-				if param.AWSSecret {
-					if smError == nil {
-						value, _, err := sm.GetValue(param.Name)
-						if err == nil {
-							result[param.Name] = convertString(value, param.valueType)
-							continue
-						}
-					}
-				}
-
-				if flg.Value.(*fl.Int64Value).IsSet() {
-					result[param.Name] = flagValue
-				} else {
-					if envValueString != "" {
-						result[param.Name] = envValue
-					} else {
-						if param.Required {
-							missing = append(missing, param.Name)
-						} else {
-							result[param.Name] = flagValue
-						}
-					}
-				}
-			}
-		case stringType:
-			{
-				if param.AWSSecret {
-					if smError == nil {
-						value, _, err := sm.GetValue(param.Name)
-						if err == nil {
-							result[param.Name] = convertString(value, param.valueType)
-							continue
-						}
-					}
-				}
-
-				if flg.Value.(*fl.StringValue).IsSet() {
-					result[param.Name] = flagValue
-				} else {
-					if envValueString != "" {
-						result[param.Name] = envValue
-					} else {
-						if param.Required {
-							missing = append(missing, param.Name)
-						} else {
-							result[param.Name] = flagValue
-						}
-					}
-				}
-			}
-		case uintType:
-			{
-				if param.AWSSecret {
-					if smError == nil {
-						value, _, err := sm.GetValue(param.Name)
-						if err == nil {
-							result[param.Name] = convertString(value, param.valueType)
-							continue
-						}
-					}
-				}
-
-				if flg.Value.(*fl.UintValue).IsSet() {
-					result[param.Name] = flagValue
-				} else {
-					if envValueString != "" {
-						result[param.Name] = envValue
-					} else {
-						if param.Required {
-							missing = append(missing, param.Name)
-						} else {
-							result[param.Name] = flagValue
-						}
-					}
-				}
-			}
-		case uint64Type:
-			{
-				if param.AWSSecret {
-					if smError == nil {
-						value, _, err := sm.GetValue(param.Name)
-						if err == nil {
-							result[param.Name] = convertString(value, param.valueType)
-							continue
-						}
-					}
-				}
-
-				if flg.Value.(*fl.Uint64Value).IsSet() {
-					result[param.Name] = flagValue
-				} else {
-					if envValueString != "" {
-						result[param.Name] = envValue
-					} else {
-						if param.Required {
-							missing = append(missing, param.Name)
-						} else {
-							result[param.Name] = flagValue
-						}
-					}
-				}
-			}
+		} else {
+			log.Printf("PARAM: %v: %T", param.Name, value)
+			result[param.Name] = value
 		}
 	}
 
