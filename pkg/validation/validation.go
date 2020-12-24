e6b89d2a632e2f9fee16a98fe04550032676237f
Dzmitry Afanasenka <dzmitry.afanasenka@itechart-group.com>
2020-12-24 15:48:50 +0300

    Added translator to overload validation errors.

--- pkg/validation/validation.go
+++ pkg/validation/validation.go
@@ -1,8 +1,28 @@
 package validation
 
-import "github.com/go-playground/validator"
+import (
+	"github.com/go-playground/locales/en"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator"
+)
 
+var uni *ut.UniversalTranslator
 var validate = validator.New()
+var Translator ut.Translator
+
+func init() {
+	enTrans := en.New()
+	uni = ut.New(enTrans, enTrans)
+	Translator, _ = uni.GetTranslator("en")
+
+	_ = validate.RegisterTranslation("required", Translator, func(ut ut.Translator) error {
+		return ut.Add("required", "{0} is required field.", true)
+	}, func(ut ut.Translator, fe validator.FieldError) string {
+		t, _ := ut.T("required", fe.Field())
+
+		return t
+	})
+}
 
 func New() *validator.Validate {
 	return validator.New()
